d0ca5d04db783549bd5e78dcb12afa008968c294
NickSettler <moiseevnikita14@gmail.com>
2022-07-02 16:09:06 +0200

    (feat) adds redux and saga

---
+++ src/redux/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducer';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  devTools: true,
+  middleware: [sagaMiddleware],
+  reducer: rootReducer,
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
