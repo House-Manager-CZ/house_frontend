adc39a30dd205d4fa9107112475e7763e494ec9a
NickSettler <moiseevnikita14@gmail.com>
2022-07-10 20:51:58 +0200

    (feat) adds create house api method

--- src/helpers/api/houses.api.ts
+++ src/helpers/api/houses.api.ts
@@ -1,9 +1,25 @@
+import { AxiosResponse } from "axios";
 import BaseApi from "./base";
 import { TApiHouse } from "./types/entities.types";
+
+export type THouseApiCreateData = {
+  name: string;
+  location: string;
+};
 
 export default class HousesApi extends BaseApi {
   public async getHouses(): Promise<Array<TApiHouse>> {
     const response = await this.axios.get<Array<TApiHouse>>("/houses");
+    return response.data;
+  }
+
+  public async createHouse(data: THouseApiCreateData): Promise<TApiHouse> {
+    const response = await this.axios.post<
+      TApiHouse,
+      AxiosResponse<TApiHouse>,
+      THouseApiCreateData
+    >("/houses", data);
+
     return response.data;
   }
 }
