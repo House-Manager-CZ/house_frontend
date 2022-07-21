f2023235894718bc31cb790958731908b1905f8f
NickSettler <moiseevnikita14@gmail.com>
2022-07-21 03:29:57 +0200

    (feat) adds api endpoint to search for users

--- src/helpers/api/users.api.ts
+++ src/helpers/api/users.api.ts
@@ -2,13 +2,20 @@ import { TApiUser } from "./types/entities.types";
 import BaseApi from "./base";
 
 export class UsersApi extends BaseApi {
+  public async getMe(): Promise<TApiUser> {
+    const response = await this.axios.get("/users/me");
+    return response.data;
+  }
+
   public async getUsers(): Promise<Array<TApiUser>> {
     const response = await this.axios.get("/users");
     return response.data;
   }
 
-  public async getMe(): Promise<TApiUser> {
-    const response = await this.axios.get("/users/me");
+  public async searchUsers(query: string): Promise<Array<TApiUser>> {
+    const response = await this.axios.get(
+      `/users/?q=${encodeURIComponent(query)}`
+    );
     return response.data;
   }
 }
