950cf62c7a8513d5c583993b7cd0e8ab48e7d7ee
NickSettler <moiseevnikita14@gmail.com>
2022-08-06 04:51:16 +0200

    (feat) adds back button to pages

--- src/pages/AddHousePage/AddHousePage.tsx
+++ src/pages/AddHousePage/AddHousePage.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { Container, Stack, Typography } from "@mui/material";
 import HouseForm from "../../components/HouseForm/HouseForm";
+import BackButton from "../../ui/BackButton/BackButton";
 
 const AddHousePage: React.FC = (): React.ReactElement => {
   return (
     <Container sx={{ mt: 2 }}>
       <Stack spacing={3}>
-        <Typography variant={"h5"}>Add house</Typography>
+        <Stack direction={"row"} spacing={2} alignItems={"center"}>
+          <BackButton />
+          <Typography variant={"h5"}>Add house</Typography>
+        </Stack>
         <HouseForm mode={"add"} />
       </Stack>
     </Container>
